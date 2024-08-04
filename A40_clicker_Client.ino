67994ca39a18eeda52a446faa7697373cfe1ab56
nati-pinyan <natipinyan@gmail.com>
2024-08-05 02:02:40 +0300

    הוספת הגדרות רשת בסיסיות לעבודה עם סרבר

---
+++ A40_clicker_Client.ino
@@ -0,0 +1,10 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <WiFiUdp.h>
+#include <ESP8266HTTPClient.h>
+
+const char *ssid = "Natipinyan";
+const char *pswd = "11223344";
+
+WiFiClient client;
+int server_port = 80;
