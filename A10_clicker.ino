e53b1730ee2b226951859fbd9279c0f81cb250ed
nati-pinyan <natipinyan@gmail.com>
2024-08-05 00:57:30 +0300

    הוספת הגדרות לכפתור. יצירת פונקציה ללחיצה על כפתור כולל ניטרול דיבאונסינג.

---
+++ A10_clicker.ino
@@ -0,0 +1,31 @@
+#define BTN D4
+
+int cntHistory = 0;
+int lastBTNval;
+unsigned long lastTimePress;
+unsigned long releaseTime;
+
+void BTN_setup()
+{
+    pinMode(BTN, INPUT_PULLUP);
+    lastBTNval = digitalRead(BTN);
+    lastTimePress = millis();
+}
+
+void BTN_loop()
+{
+    int correntBTNval = digitalRead(BTN);
+    if ((correntBTNval == LOW) && (lastBTNval == HIGH) && (millis() - lastTimePress > 50))
+    {
+        lastTimePress = millis();
+    }
+    else if ((correntBTNval == HIGH) && (lastBTNval == LOW) && (millis() - lastTimePress > 5))
+    {
+        releaseTime = millis();
+        Serial.print("press time => ");
+        Serial.println(releaseTime - lastTimePress);
+        Check_Time(releaseTime - lastTimePress);
+        lastTimePress = releaseTime;
+    }
+    lastBTNval = correntBTNval;
+}
