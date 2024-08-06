ecea234a819c4869c8c42ff269e97f997cd702dd
nati-pinyan <natipinyan@gmail.com>
2024-08-06 15:55:26 +0300

    הוספת קובץ לעיצוב הדף.

--- A70_clicker_Server.ino
+++ A70_clicker_Server.ino
@@ -31,25 +31,7 @@ void handleRoot()
 {
   char HTML[2000] = "";
   Head(HTML);
-  strcat(HTML, "<title>Document</title>");
-  strcat(HTML, "<style>");
-  strcat(HTML, "h2{");
-  strcat(HTML, "text-align: center;");
-  strcat(HTML, "}");
-  strcat(HTML, "table {");
-  strcat(HTML, " font-family: arial, sans-serif;");
-  strcat(HTML, "border-collapse: collapse;");
-  strcat(HTML, "width: 100%;");
-  strcat(HTML, "}");
-  strcat(HTML, "td, th {");
-  strcat(HTML, "border: 1px solid #dddddd;");
-  strcat(HTML, "text-align: left;");
-  strcat(HTML, "padding: 8px;");
-  strcat(HTML, "}");
-  strcat(HTML, "tr:nth-child(even) {");
-  strcat(HTML, "background-color: #dddddd;");
-  strcat(HTML, "}");
-  strcat(HTML, "</style>");
+
   strcat(HTML, "</head>");
   strcat(HTML, "<body>");
   strcat(HTML, "<h2>הסטוריית לחיצות</h2>");
--- A80_clicker_HtmlBuild.ino
+++ A80_clicker_HtmlBuild.ino
@@ -5,4 +5,6 @@ void Head(char HTML[])
     strcat(HTML, "<head>");
     strcat(HTML, "<meta charset=\"UTF-8\">");
     strcat(HTML, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+    strcat(HTML, "<title>Document</title>");
+    Style(HTML);
 }
