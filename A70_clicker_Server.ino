61165afdb8546f6acb083639131fe778dca06dcb
nati-pinyan <natipinyan@gmail.com>
2024-08-06 15:52:51 +0300

    הוספת קובץ שישמש לבניית דף ה html ההתחלתית.

--- A70_clicker_Server.ino
+++ A70_clicker_Server.ino
@@ -30,11 +30,7 @@ void wifiServer_loop()
 void handleRoot()
 {
   char HTML[2000] = "";
-  strcat(HTML, "<!DOCTYPE html>");
-  strcat(HTML, "<html lang=\"en\">");
-  strcat(HTML, "<head>");
-  strcat(HTML, "<meta charset=\"UTF-8\">");
-  strcat(HTML, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+  Head(HTML);
   strcat(HTML, "<title>Document</title>");
   strcat(HTML, "<style>");
   strcat(HTML, "h2{");
---
+++ A80_clicker_HtmlBuild.ino
@@ -0,0 +1,8 @@
+void Head(char HTML[])
+{
+    strcat(HTML, "<!DOCTYPE html>");
+    strcat(HTML, "<html lang=\"en\">");
+    strcat(HTML, "<head>");
+    strcat(HTML, "<meta charset=\"UTF-8\">");
+    strcat(HTML, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+}
