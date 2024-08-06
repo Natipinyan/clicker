151e7a0f734b9d3c05ef17fbd7fd0063aeffb126
nati-pinyan <natipinyan@gmail.com>
2024-08-06 21:32:08 +0300

    תיקון באגים בתצוגת הטבלה.

--- A90_clicker_HtmlBody.ino
+++ A90_clicker_HtmlBody.ino
@@ -2,7 +2,6 @@ void Body(char HTML[])
 {
   strcat(HTML, "<table>");
   strcat(HTML, "<tr>");
-  strcat(HTML, "<th>Press id</th>");
   strcat(HTML, "<th>Press Resulte</th>");
   strcat(HTML, "<th>Broke A Record</th>");
   strcat(HTML, "</tr>");
